9f09678b2336a07e40c3fd01882373cb7061dc47
donguseo <donguseo@jobis.co>
2025-08-31 21:32:07 +0900

    feat: index 추가

--- src/main/kotlin/seo/dongu/category/entity/CategoryTreeEntity.kt
+++ src/main/kotlin/seo/dongu/category/entity/CategoryTreeEntity.kt
@@ -6,7 +6,13 @@ import org.springframework.format.annotation.DateTimeFormat
 import java.time.LocalDateTime
 
 @Entity
-@Table(name = "category_tree")
+@Table(
+  name = "category_tree",
+  indexes = [
+    // ancdestor_id는 primary key 생성 시 자동 multi 인덱스 생성되어 decendant_id만 단일 인덱스 생성
+    Index(name = "idx_descendant_id", columnList = "descendant_id")
+  ]
+)
 @IdClass(CategoryTreeId::class)
 class CategoryTreeEntity(
   @Id
